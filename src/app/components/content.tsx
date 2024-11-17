f17a08d8f1826e478b7c135c8b289d2f7c1de2f5
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-11-17 16:27:34 +0100

    added a custom scrollbar

--- src/app/components/content.tsx
+++ src/app/components/content.tsx
@@ -24,8 +24,12 @@ const Content = () => {
     <main className="h-full flex justify-around items-center">
       <img src="assets/img/chuckNorrisComic.png" className="max-w-[30.438rem] h-auto comic" alt="Image of a Chuck Norris comic"/>
 
-      <div className="w-[37.5rem] h-[18.75rem] flex flex-col justify-between items-center quote-container">
-        <p className="text-lg italic">"{quote}"</p>
+      <div className="w-[37.5rem] h-[12.5rem] flex flex-col justify-between items-center quote-and-btn-container">
+        {/* <div className="w-[37.5rem] h-[18.75rem] flex flex-col justify-between items-center quote-container"> */}
+        <div className="quote-container">
+          <p className="text-lg italic">"{quote}"</p>
+        </div>
+
         <button onClick={fetchQuote} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
           Load new quote
         </button>
